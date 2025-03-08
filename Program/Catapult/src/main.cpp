f0db44e7b0852265d66c500e897e81f49c67dfe0
DESKTOP-4GHA6HA\sergi <sergi10carrasco@gmail.com>
2025-03-08 19:13:24 +0100

    Inventor and PCB Update. Full program started.

--- Program/Catapult/src/main.cpp
+++ Program/Catapult/src/main.cpp
@@ -16,12 +16,32 @@ void setup()
   stepper.setAcceleration(1000);
   stepper.setSpeed(500);
   pinMode(10, INPUT_PULLUP);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT_PULLUP);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
 }
 void loop()
 {
   if (digitalRead(10) == LOW)
   {
-    miServo.write(70); // Mover el servo a 0 grados
+    miServo.write(0); // Mover el servo a 90 grados
+    delay(1000);
+    while (digitalRead(9) == HIGH)
+    {
+      digitalWrite(7, LOW);
+      digitalWrite(8, HIGH);
+    }
+    delay(150);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    miServo.write(65); // Mover el servo a 0 grados
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    delay(10000);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
     stepper.moveTo(2 * 2048);
     while (stepper.distanceToGo() != 0)
       stepper.run();
@@ -29,10 +49,8 @@ void loop()
     stepper.moveTo(0);
     while (stepper.distanceToGo() != 0)
       stepper.run();
-    delay(1000);      // Esperar 1 segundo
-    miServo.write(0); // Mover el servo a 90 grados
-    delay(1000);
+    delay(1000); // Esperar 1 segundo
   }
-  miServo.write(0); // Mover el servo a 90 grados
+  miServo.write(65); // Mover el servo a 90 grados
   delay(100);
 }
